1251efd1195ef8614d89132d08653dc73f81e441
kamilest <stankeviciute.kamile@gmail.com>
2019-12-01 12:06:36 +0000

    Pipeline.

--- final_assignment/training/training.py
+++ final_assignment/training/training.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.impute import SimpleImputer
 from sklearn.base import TransformerMixin, BaseEstimator 
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.preprocessing import LabelBinarizer, OneHotEncoder, StandardScaler, MinMaxScaler
+from sklearn.preprocessing import LabelBinarizer, OrdinalEncoder, OneHotEncoder, StandardScaler, MinMaxScaler
 
 # from sklearn.model_selection import cross_val_score
 # from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
@@ -85,28 +85,34 @@ diabetic_labels = strat_train_set["readmitted"].copy()
 
 # Preprocessing pipeline
 diabetic_num_to_cat_features = ['admission_type_id', 'discharge_disposition_id','admission_source_id']
-diabetic_cat_to_num_features = ['age', 'max_glu_serum', 'A1Cresult']
+diabetic_cat_to_num_features = ['max_glu_serum', 'A1Cresult']
 
 diabetic_num_features = ['time_in_hospital', 'num_lab_procedures','num_procedures', 'num_medications', 'number_outpatient', 'number_emergency', 'number_inpatient', 'number_diagnoses']
 
 diabetic_cat_features = ['race', 'gender', 'medical_specialty', 'diag_1', 'diag_2', 'diag_3', 'metformin', 'repaglinide', 'nateglinide','chlorpropamide', 'glimepiride', 'acetohexamide', 'glipizide', 'glyburide','tolbutamide', 'pioglitazone', 'rosiglitazone', 'acarbose', 'miglitol','troglitazone', 'tolazamide', 'examide', 'citoglipton', 'insulin', 'glyburide-metformin', 'glipizide-metformin', 'glimepiride-pioglitazone','metformin-rosiglitazone', 'metformin-pioglitazone', 'change', 'diabetesMed']
 
 num_pipeline = Pipeline([
-        ('selector', DataFrameSelector(diabetic_num_features)),
-        ('imputer', SimpleImputer(strategy="median")),
-#         ('std_scaler', StandardScaler()),
-    ])
+    ('selector', DataFrameSelector(diabetic_num_features)),
+    ('imputer', SimpleImputer(strategy="median")),
+    ('std_scaler', StandardScaler()),
+])
+
+age_pipeline = Pipeline([
+    ('selector', DataFrameSelector(['age'])),
+    ('ordinal_encoder', OrdinalEncoder(categories=[['[0-10)', '[10-20)', '[20-30)', '[30-40)', '[40-50)', '[50-60)', '[60-70)', '[70-80)', '[80-90)', '[90-100)']])),
+])
 
 cat_pipeline = Pipeline([
-        ('selector', DataFrameSelector(diabetic_cat_features)),
-        ('imputer', SimpleImputer(strategy="constant")),
-#         ('one_hot', OneHotEncoder()),
-    ])
+    ('selector', DataFrameSelector(diabetic_num_to_cat_features + diabetic_cat_features + diabetic_cat_to_num_features)),
+    ('imputer', SimpleImputer(strategy="constant")),
+    ('encoder', OneHotEncoder(categories='auto'))
 
+])
 
 full_pipeline = FeatureUnion(transformer_list=[
-        ("num_pipeline", num_pipeline),
-        ("cat_pipeline", cat_pipeline),
-    ])
+    ("num_pipeline", num_pipeline),
+    ('age_pipeline', age_pipeline),
+    ("cat_pipeline", cat_pipeline),
+])
 
 diabetic_prepared = full_pipeline.fit_transform(diabetic_features)
