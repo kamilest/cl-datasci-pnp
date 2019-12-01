eef6a4c6ec8cf7717a748fa5a36ba99ac12805f9
kamilest <stankeviciute.kamile@gmail.com>
2019-12-01 15:14:24 +0000

    Low accuracy multi-class

--- final_assignment/training/training.py
+++ final_assignment/training/training.py
@@ -12,14 +12,19 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import LabelBinarizer, OrdinalEncoder, OneHotEncoder, StandardScaler, MinMaxScaler
 
-# from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.naive_bayes import GaussianNB
+
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+
 # from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 
 
 # from sklearn.preprocessing import PolynomialFeatures
 
-# from sklearn.linear_model import LinearRegression
+
 # from sklearn.tree import DecisionTreeRegressor
 # from sklearn.ensemble import RandomForestRegressor
 # from sklearn.metrics import mean_squared_error
@@ -27,7 +32,6 @@ from sklearn.preprocessing import LabelBinarizer, OrdinalEncoder, OneHotEncoder,
 # from sklearn.ensemble import VotingClassifier
 
 # from sklearn.ensemble import RandomForestClassifier
-# from sklearn.linear_model import LogisticRegression
 # from sklearn.svm import SVC
 
 # from sklearn.metrics import accuracy_score
@@ -83,15 +87,18 @@ for train_index, test_index in split.split(diabetic, diabetic['readmitted']):
 diabetic_features = strat_train_set.drop("readmitted", axis=1)
 diabetic_labels = strat_train_set["readmitted"].copy()
 
-# Preprocessing pipeline
+# PREPROCESSING PIPELINE
 diabetic_num_to_cat_features = ['admission_type_id', 'discharge_disposition_id','admission_source_id']
-diabetic_cat_to_num_features = ['max_glu_serum', 'A1Cresult']
+diabetic_cat_to_num_features = []
 
-diabetic_num_features = ['time_in_hospital', 'num_lab_procedures','num_procedures', 'num_medications', 'number_outpatient', 'number_emergency', 'number_inpatient', 'number_diagnoses']
+# diabetic_num_features = ['time_in_hospital', 'num_lab_procedures','num_procedures', 'num_medications', 'number_outpatient', 'number_emergency', 'number_inpatient', 'number_diagnoses']
 
-diabetic_cat_features = ['race', 'gender', 'medical_specialty', 'diag_1', 'diag_2', 'diag_3', 'metformin', 'repaglinide', 'nateglinide','chlorpropamide', 'glimepiride', 'acetohexamide', 'glipizide', 'glyburide','tolbutamide', 'pioglitazone', 'rosiglitazone', 'acarbose', 'miglitol','troglitazone', 'tolazamide', 'examide', 'citoglipton', 'insulin', 'glyburide-metformin', 'glipizide-metformin', 'glimepiride-pioglitazone','metformin-rosiglitazone', 'metformin-pioglitazone', 'change', 'diabetesMed']
+diabetic_num_features = ['time_in_hospital', 'num_lab_procedures','num_procedures', 'num_medications', 'number_diagnoses']
 
-diabetic_cat_features_no_drugs = ['race', 'gender', 'medical_specialty', 'diag_1', 'diag_2', 'diag_3', 'change', 'diabetesMed']
+# diabetic_cat_features = ['race', 'gender', 'medical_specialty', 'diag_1', 'diag_2', 'diag_3', 'metformin', 'repaglinide', 'nateglinide','chlorpropamide', 'glimepiride', 'acetohexamide', 'glipizide', 'glyburide','tolbutamide', 'pioglitazone', 'rosiglitazone', 'acarbose', 'miglitol','troglitazone', 'tolazamide', 'examide', 'citoglipton', 'insulin', 'glyburide-metformin', 'glipizide-metformin', 'glimepiride-pioglitazone','metformin-rosiglitazone', 'metformin-pioglitazone', 'change', 'diabetesMed']
+
+diabetic_cat_features_no_drugs = ['gender','change', 'diabetesMed', 'max_glu_serum', 'A1Cresult']
+diabetic_diag_features = ['diag_1', 'diag_2', 'diag_3']
 
 num_pipeline = Pipeline([
     ('selector', DataFrameSelector(diabetic_num_features)),
@@ -105,18 +112,83 @@ age_pipeline = Pipeline([
     ('std_scaler', StandardScaler()),
 ])
 
+class DiagEncoder(TransformerMixin):
+    def __init__(self, *args, **kwargs):
+        self.encoder = OneHotEncoder(*args, **kwargs, categories='auto', sparse=False)
+    def fit(self, X, y=0):
+        X = [[self.get_type(icd_str) for icd_str in x] for x in X]
+        self.encoder.fit(X)
+        return self
+    def transform(self, X, y=0):
+        X = [[self.get_type(icd_str) for icd_str in x] for x in X]
+        return self.encoder.transform(X)
+    def get_type(self, icd_str):
+        if isinstance(icd_str, float) and math.isnan(icd_str):
+            return('missing')
+        elif icd_str.isnumeric():
+            icd = int(icd_str)
+        elif icd_str[:3].isnumeric():
+            icd = int(icd_str[:3])
+        else:
+            return 'other'
+
+        if (icd >= 390 and icd <= 459 or icd == 785):
+            return 'circulatory'
+        elif (icd >= 520 and icd <= 579 or icd == 787):
+            return 'digestive'
+        elif (icd >= 580 and icd <= 629 or icd == 788):
+            return 'genitourinary'
+        elif (icd == 250):
+            return 'diabetes'
+        elif (icd >= 800 and icd <= 999):
+            return 'injury'
+        elif (icd >= 710 and icd <= 739):
+            return 'musculoskeletal'
+        elif (icd >= 140 and icd <= 239):
+            return 'neoplasms'
+        elif (icd >= 460 and icd <= 519 or icd == 786):
+            return 'respiratory'
+        else:
+            return 'other'
+        
+
+diag_pipeline = Pipeline([
+    ('selector', DataFrameSelector(diabetic_diag_features)),
+    ('diag_encoder', DiagEncoder()),
+])
+
 cat_pipeline = Pipeline([
     ('selector', DataFrameSelector(diabetic_num_to_cat_features + diabetic_cat_features_no_drugs + diabetic_cat_to_num_features)),
     ('imputer', SimpleImputer(strategy='constant')),
-    ('encoder', OneHotEncoder(categories='auto'))
+    ('encoder', OneHotEncoder(categories='auto', sparse=False))
 
 ])
 
 full_pipeline = FeatureUnion(transformer_list=[
     ("num_pipeline", num_pipeline),
     ('age_pipeline', age_pipeline),
+    ('diag_pipeline', diag_pipeline),
     ("cat_pipeline", cat_pipeline),
 ])
 
 diabetic_prepared = full_pipeline.fit_transform(diabetic_features)
 
+
+# MACHINE LEARNING ALGORITHMS
+X_train = diabetic_prepared
+print(X_train.shape)
+y_train = diabetic_labels
+
+
+# MULTI-CLASS CLASSIFIERS 
+sgd = SGDClassifier()
+cv_sgd = cross_val_score(sgd, X_train, y_train, cv=5, scoring='accuracy')
+print(cv_sgd, np.mean(cv_sgd))
+
+log_reg = LogisticRegression(multi_class='ovr', solver='liblinear')
+cv_log_reg = cross_val_score(log_reg, X_train, y_train, cv=5, scoring='accuracy')
+print(cv_log_reg, np.mean(cv_log_reg))
+
+gnb = GaussianNB()
+cv_gnb = cross_val_score(gnb, X_train, y_train, cv=5, scoring='accuracy')
+print(cv_gnb, np.mean(cv_gnb))
