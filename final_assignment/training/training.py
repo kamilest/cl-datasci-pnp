aa1d81d2488bccbe5553bbe85f7d995d96f758ad
kamilest <stankeviciute.kamile@gmail.com>
2019-12-01 16:38:37 +0000

    Finish dummy classifier.

--- final_assignment/training/training.py
+++ final_assignment/training/training.py
@@ -185,17 +185,17 @@ y_train = diabetic_labels
 
 # MULTI-CLASS CLASSIFIERS 
 sgd = SGDClassifier()
-cv_sgd = cross_val_score(sgd, X_train, y_train, cv=5, scoring='accuracy')
+cv_sgd = cross_val_score(sgd, X_train, y_train, cv=10, scoring='accuracy')
 print(cv_sgd, np.mean(cv_sgd))
 
 log_reg = LogisticRegression(multi_class='ovr', solver='liblinear')
-cv_log_reg = cross_val_score(log_reg, X_train, y_train, cv=5, scoring='accuracy')
+cv_log_reg = cross_val_score(log_reg, X_train, y_train, cv=10, scoring='accuracy')
 print(cv_log_reg, np.mean(cv_log_reg))
 
 gnb = GaussianNB()
-cv_gnb = cross_val_score(gnb, X_train, y_train, cv=5, scoring='accuracy')
+cv_gnb = cross_val_score(gnb, X_train, y_train, cv=10, scoring='accuracy')
 print(cv_gnb, np.mean(cv_gnb))
 
-baseline = DummyClassifier()
-cv_baseline = cross_val_score(baseline, X_train, y_train, cv=5, scoring='accuracy')
+baseline = DummyClassifier(random_state=42)
+cv_baseline = cross_val_score(baseline, X_train, y_train, cv=10, scoring='accuracy')
 print(cv_baseline, np.mean(cv_baseline))
